a1aa32ca4ee11076dd447d554ed7afd62f630aae
Jakub <jakub.kicmal@gmail.com>
2017-10-13 10:58:11 +0200

    asd

--- SafeOpener/Assets/Scripts/SafeRotate1.cs
+++ SafeOpener/Assets/Scripts/SafeRotate1.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class SafeRotate1 : MonoBehaviour {
 
 	void OnMouseDrag(){
-		float rotX = Input.GetAxis ("Mouse X") * 20 * Mathf.Deg2Rad;
-		float rotY = Input.GetAxis ("Mouse Y") * 20 * Mathf.Deg2Rad;
+		//float rotX = Input.GetAxis ("Mouse X") * 20 * Mathf.Deg2Rad;
+		//float rotY = Input.GetAxis ("Mouse Y") * 20 * Mathf.Deg2Rad;
+        //Debug.Log(rotX + " " + rotY);
 
-		Quaternion rotation = Quaternion.Euler (transform.position.x - Input.mousePosition.x, transform.position.y - Input.mousePosition.y, transform.position.z - Input.mousePosition.z);
+		Quaternion rotation = Quaternion.Euler (0, 0, transform.position.z - Input.mousePosition.z);
 		transform.rotation = rotation;
 	}
+    private void OnMouseOver()
+    {
+        float rotX = Input.mousePosition.x;
+        float rotY = Input.mousePosition.y;
+        Debug.Log(rotX + " " + rotY);
+    }
 }
