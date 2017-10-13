466999ae235fca450ee65f7af5a4f1401d28e620
Jakub <jakub.kicmal@gmail.com>
2017-10-13 17:24:56 +0200

    asd

--- SafeOpener/Assets/Scripts/SafeRotate.cs
+++ SafeOpener/Assets/Scripts/SafeRotate.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class SafeRotate : MonoBehaviour {
 
-	void Update(){
-		Vector2 zeroMouse = new Vector2 (Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height/2+40);
-		Vector2 center = new Vector2 (transform.position.x,transform.position.y);
-		float angle = Vector2.SignedAngle(center, zeroMouse);
-		Quaternion rotation = Quaternion.Euler (0, 0, angle);
-		transform.rotation = rotation;
+    float currentDirection;
+    int direction = 1;
+    float distance;
 
-		Debug.Log (angle);
-	//	Debug.Log ("X:"+Input.mousePosition.x+" Y: "+Input.mousePosition.y);
-	//	Debug.Log("X2:"+(Input.mousePosition.x-Screen.width/2)+" Y2: "+(Input.mousePosition.y-Screen.height/2));
-		}
+    private void Awake()
+    {
+        currentDirection = 0f;
+    }
 
+    void Update()
+    {
+        if(!((currentDirection - 1f) < this.transform.eulerAngles.z && (currentDirection + 1f) > this.transform.eulerAngles.z))
+        {
+            float currentDistance = Mathf.Abs(this.transform.rotation.eulerAngles.z - currentDirection) * 1.5f;
+            this.transform.Rotate(new Vector3(0, 0, direction) * Time.deltaTime * currentDistance);
+            Debug.Log(distance);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Vector2 zeroMouse = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2 + 40);
+        Vector2 center = new Vector2(transform.position.x, transform.position.y);
+        float angle = Vector2.SignedAngle(Vector2.up, zeroMouse);
+        currentDirection = (angle < 0) ? 360-Mathf.Abs(angle) : angle;
+        //Debug.Log(currentDirection);
+        if(currentDirection < this.transform.rotation.eulerAngles.z)
+        {
+            distance = Mathf.Abs(this.transform.rotation.eulerAngles.z - currentDirection) + 360;
+        } else
+        {
+            distance = Mathf.Abs(this.transform.rotation.eulerAngles.z - currentDirection);
+        }
+        if(distance > 270)
+        {
+           direction = -1;
+        } else
+        {
+            direction = 1;
+        }
+    }
 
 }
