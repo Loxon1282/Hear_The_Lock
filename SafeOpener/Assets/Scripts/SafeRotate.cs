85226c32c9cc3f58e7211b1ffd374c1a996892d0
Loxon1282 <loxon1282@gmail.com>
2017-10-13 16:32:02 +0200

    hehe

--- SafeOpener/Assets/Scripts/SafeRotate.cs
+++ SafeOpener/Assets/Scripts/SafeRotate.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SafeRotate : MonoBehaviour {
 
 	void Update(){
-		Vector2 zeroMouse = new Vector2 (Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height/2+40);
+		Vector2 zeroMouse = new Vector2 (Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height/2+48);
 		Vector2 center = new Vector2 (transform.position.x,transform.position.y);
 		float angle = Vector2.SignedAngle(center, zeroMouse);
 		Quaternion rotation = Quaternion.Euler (0, 0, angle);
