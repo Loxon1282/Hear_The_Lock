90f4a81502d9a5383fd4edabda21da60dd54d536
Loxon1282 <loxon1282@gmail.com>
2017-10-13 09:13:45 +0200

    **C*

---
+++ SafeOpener/Assets/Scripts/SafeRotate1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeRotate1 : MonoBehaviour {
+
+	void OnMouseDrag(){
+		float rotX = Input.GetAxis ("Mouse X") * 10 * Mathf.Deg2Rad;
+		float rotY = Input.GetAxis ("Mouse Y") * 10 * Mathf.Deg2Rad;
+
+		transform.RotateAround (Vector3.back, rotX);
+	}
+}
