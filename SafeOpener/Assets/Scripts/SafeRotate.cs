0b912494b8f593f7bcca4c670bc1d0599145815a
Jakub <jakub.kicmal@gmail.com>
2017-10-14 17:21:16 +0200

    c

--- SafeOpener/Assets/Scripts/SafeRotate.cs
+++ SafeOpener/Assets/Scripts/SafeRotate.cs
@@ -7,48 +7,86 @@ public class SafeRotate : MonoBehaviour {
     float currentDirection;
     int direction = 1;
     float distance;
+    public ButtonStates buttonStates = new ButtonStates();
 	
     private void Awake()
     {
-        currentDirection = 0f;
+        currentDirection = this.transform.eulerAngles.z;
     }
 
-    void Update()
+    public void Update()
     {
-        if(!((currentDirection - 1f) < this.transform.eulerAngles.z && (currentDirection + 1f) > this.transform.eulerAngles.z))
+        if (GameObject.Find("GameplayController").GetComponent<GameplayController>().startingTimer <= 0)
         {
-            float currentDistance = Mathf.Abs(this.transform.rotation.eulerAngles.z - currentDirection) * 5f;
-            this.transform.Rotate(new Vector3(0, 0, direction) * Time.deltaTime * currentDistance);
-            Debug.Log(distance);
+
+            //this.transform.rotation = Quaternion.Euler(0, 0, currentDirection);
+
+            if (!(this.transform.eulerAngles.z >= (currentDirection - 1) && this.transform.eulerAngles.z <= (currentDirection + 1)))
+            {
+                float currentDistanceA, currentDistanceB, currentSpeed;
+                currentDistanceA = Mathf.Abs(currentDirection - this.transform.eulerAngles.z);
+                currentDistanceB = 360 - currentDistanceA;
+                //Debug.Log(currentDirection);
+                //Debug.Log("A: " + currentDistanceA);
+                //Debug.Log("B: " + currentDistanceB);
+                currentSpeed = (currentDistanceA >= currentDistanceB) ? currentDistanceB : currentDistanceA;
+                this.transform.Rotate(new Vector3(0, 0, direction * currentSpeed * 15f * Time.fixedDeltaTime));
+            }
+            if (buttonStates.leftArrow)
+            {
+                currentDirection = currentDirection + 5f * Time.fixedDeltaTime;
+                CalculateDistance();
+            }
+            if (buttonStates.rightArrow)
+            {
+                currentDirection = currentDirection - 5f * Time.fixedDeltaTime;
+                CalculateDistance();
+            }
         }
     }
 
-    private void OnMouseDrag()
+    public void CalculatePositions()
     {
         Vector2 zeroMouse = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2 + 40);
         float angle = Vector2.SignedAngle(Vector2.up, zeroMouse);
-        currentDirection = (angle < 0) ? 360-Mathf.Abs(angle) : angle;
-        //Debug.Log(currentDirection);
-        if(currentDirection < this.transform.rotation.eulerAngles.z)
-        {
-            distance = Mathf.Abs(this.transform.rotation.eulerAngles.z - currentDirection) + 360;
-        } else
-        {
-            distance = Mathf.Abs(this.transform.rotation.eulerAngles.z - currentDirection);
-        }
-        if(distance >= 370)
+        currentDirection = (angle < 0) ? Mathf.Round(360-Mathf.Abs(angle)) : Mathf.Round(angle);
+        CalculateDistance();
+    }
+    public void CalculateDistance()
+    {
+        distance = currentDirection - this.transform.eulerAngles.z;
+        if (Mathf.Abs(distance) < 180 && Mathf.Sign(distance) == -1)
         {
             direction = -1;
-        } else if(distance >= 360)
+        }
+        else if (Mathf.Abs(distance) > 180 && Mathf.Sign(distance) == 1)
         {
-            direction = 1;
-        } else if(distance > 240)
-        {
-           direction = -1;
-        } else
+            direction = -1;
+        }
+        else
         {
             direction = 1;
         }
     }
-
+    public void RotateLeft()
+    {
+        buttonStates.leftArrow = true;
+    }
+    public void RotateRight()
+    {
+        buttonStates.rightArrow = true;
+    }
+    public void ResetButtons()
+    {
+        buttonStates.Reset();
+    }
+    public struct ButtonStates {
+        public bool leftArrow { get; set; }
+        public bool rightArrow { get; set; }
+        public void Reset()
+        {
+            this.leftArrow = false;
+            this.rightArrow = false;
+        }
+    }
 }
