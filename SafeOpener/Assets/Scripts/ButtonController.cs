166e580985739e44d76edd31d2503ab0ec5b5f74
Loxon1282 <loxon1282@gmail.com>
2017-10-19 16:20:54 +0200

    lol

--- SafeOpener/Assets/Scripts/ButtonController.cs
+++ SafeOpener/Assets/Scripts/ButtonController.cs
@@ -25,4 +25,32 @@ public class ButtonController : MonoBehaviour {
 	public void ReloadLvl() {
 		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
 	}
+
+	public void Link(string link){
+		if(link=="KindOfStudio"){
+		Application.OpenURL ("http://KindOfStudio.pl/");
+		}
+		else
+		Application.OpenURL ("https://web.facebook.com/KindOfStudio/");
+	
+	}
+	public void Donate_Click()
+	{
+		string url = "";
+
+		string business     = "lukasz.kot1282@gmail.com";  // your paypal email
+		string description  = "Wspieraj%20inne%20projekty!";            // '%20' represents a space. remember HTML!
+		string country      = "PL";                  // AU, US, etc.
+		string currency     = "PLN";                 // AUD, USD, etc.
+
+		url += "https://www.paypal.com/cgi-bin/webscr" +
+			"?cmd=" + "_donations" +
+			"&business=" + business +
+			"&lc=" + country +
+			"&item_name=" + description +
+			"&currency_code=" + currency +
+			"&bn=" + "PP%2dDonationsBF";
+
+		Application.OpenURL (url);
+	}
 }
